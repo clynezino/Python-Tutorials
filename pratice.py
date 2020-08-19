60d5973d333eab5b0da3f22487e9a2b5fadd5503
Clyne <christopherokwuagwu2000@gmail.com>
2020-08-19 09:01:25 -0700

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,24 @@
+name = "chris paul"
+
+
+if len(name) < 3:
+    print("Name must be at least 3 character long")
+elif len(name) > 50
+    print("Name must be a maxium of 50 character long")
+else:
+    print("Name looks good")        
+
+
+
+
+
+
+
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price 
+print(f"Down payment: ${down_payment}")         
